134c3f40460d65c189dd8412b5c5f8f8895d04c5
Andrew <andrew.lizon12@gmail.com>
2024-10-08 16:23:42 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -16,20 +16,20 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-// router.get('/restore-user', (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+router.get('/restore-user', (req, res) => {
+    return res.json(req.user);
+  }
+);
 
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
 // // GET /api/require-auth
 // router.get('/require-auth',requireAuth, (req, res) => {
