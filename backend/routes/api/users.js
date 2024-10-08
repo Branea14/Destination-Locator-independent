e774de3fed7b616199bb105f2c68ecfe4cbf28a7
Andrew <andrew.lizon12@gmail.com>
2024-10-08 11:31:13 -0700

    Spacing correction

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,9 +6,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 // Sign up
-router.post(
-    '/',
-    async (req, res) => {
+router.post('/', async (req, res) => {
       const { email, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({ email, username, hashedPassword });
