e662730215380648b9bfb5c1542f9caa977f5541
Erika <brandonerika96@gmail.com>
2024-12-01 10:56:01 -0500

    close dropdown menu when login or signup modals open and on logout

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -9,34 +9,6 @@ import './Navigation.css';
 const Navigation = ({isLoaded}) => {
     const user = useSelector((state) => state.session.user);
 
-    // let sessionLinks;
-    // if (user) {
-    //     sessionLinks = (
-    //         <li>
-    //             <ProfileButton user={user} />
-    //         </li>
-    //     );
-    //  } else {
-    //     sessionLinks = (
-    //         <>
-    //             <li>
-    //                 {/* <NavLink to="/login">Log In</NavLink> */}
-    //                 <OpenModalButton
-    //                     buttonText='Log In'
-    //                     modalComponent={<LoginFormModal />}
-    //                 />
-    //             </li>
-                // <li>
-                //     {/* <NavLink to="/signup">Sign Up</NavLink> */}
-                //     <OpenModalButton
-                //         buttonText='Sign Up'
-                //         modalComponent={<SignupFormModal />}
-                //     />
-                // </li>
-    //         </>
-    //     );
-    // }
-
     return (
         <ul>
             <li>
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -28,9 +28,12 @@ const ProfileButton = ({user}) => {
         return () => document.removeEventListener('click', closeMenu);
     }, [showMenu]);
 
+    const closeMenu = () => setShowMenu(false);
+
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout())
+        closeMenu();
     }
 
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -53,12 +56,14 @@ const ProfileButton = ({user}) => {
                         <li>
                             <OpenModalButton
                                 buttonText='Log In'
+                                onButtonClick={closeMenu}
                                 modalComponent={<LoginFormModal />}
                             />
                         </li>
                         <li>
                             <OpenModalButton
                                 buttonText='Sign Up'
+                                onButtonClick={closeMenu}
                                 modalComponent={<SignupFormModal />}
                             />
                         </li>
