c96457d79f388ab945aded6e90e97a4469c3d403
Erika <brandonerika96@gmail.com>
2024-12-15 12:14:52 -0500

    manage spots done

--- frontend/src/components/Spots/ManageSpots.jsx
+++ frontend/src/components/Spots/ManageSpots.jsx
@@ -5,6 +5,7 @@ import { getAllSpots } from "../../store/spots";
 import { useModal } from "../../context/Modal";
 import DeleteModal from "./DeleteModal";
 import { removeSpot } from "../../store/spots";
+import "./ManageSpots.css"
 
 const ManageSpots = () => {
     const navigate = useNavigate();
@@ -31,36 +32,62 @@ const ManageSpots = () => {
     // if (!allSpots ) return <p>Loading manage spots</p>
     const userSpots = allSpots?.filter(spot => spot.ownerId === currUser)
 
-
     return (
-
-        <>
-            <h1>Manage Your Spots</h1>
-            <button onClick={handleNewSpotButton}>Create a New Spot</button>
-            {userSpots?.length > 0 ? (
-                userSpots.map((spot, index) => (
-                    <div className='spot-container' key={index}>
-                        <div onClick={() => navigate(`/spots/${spot.id}`)}>
-                            {/* <p className="spot-name-hover">{spot.name}</p> */}
-                            <img src={spot.previewImage} alt=''></img>
-                            <p>
-                                {spot.avgRating
-                                    ? `★ ${spot.avgRating}`
-                                    : 'NEW!'
-                                }
-                            </p>
-                            <p>${spot.price} night</p>
-                            <p>{spot.city}, {spot.state}</p>
-
-                        </div>
-                        <button onClick={() => navigate(`/spots/${spot.id}/edit`)}>Update</button>
-                        <button onClick={() => openModal(<DeleteModal spotId={spot.id} handleDelete={() => handleDeleteButton(spot.id)}/>)}>Delete</button>
-                    </div>
-                ))
-            ) : (
-                <p>You don&apos;t have any spots listed yet.</p>
-            )}
-        </>
+        <div className="manage-spots">
+            <div className="manage-spots-header">Manage Your Spots</div>
+            <button className='Create-Button' onClick={handleNewSpotButton}>Create a New Spot</button>
+            <div className="container">
+                {userSpots?.length > 0 ? (
+                    userSpots.map((spot, index) => (
+                        <>
+                            <div className='spot-container' key={index} onClick={() => navigate(`/spots/${spot.id}`)}>
+                                {/* <div onClick={() => navigate(`/spots/${spot.id}`)}> */}
+                                    {/* <p className="spot-name-hover">{spot.name}</p> */}
+                                    <img className='spot-image' src={spot.previewImage} alt=''></img>
+                                    <div className="spot-details">
+                                        <div className="spot-details-inside-container">
+                                            <p className="spot-location">{spot.city}, {spot.state}</p>
+                                            <p className="spot-rating">
+                                                {spot.avgRating
+                                                    ? `★ ${spot.avgRating.toFixed(1)}`
+                                                    : 'NEW!'
+                                                }
+                                            </p>
+                                        </div>
+                                        <div className="spot-price-outside-container">
+                                            <p className="spot-price"><span className="bold-price">${spot.price}</span> night</p>
+                                        </div>
+                                    </div>
+                                    <div className="manage-spot-button">
+                                        <button className='manage-buttons' onClick={(e) => {
+                                            e.stopPropagation();
+                                            navigate(`/spots/${spot.id}/edit`);
+                                        }}>
+                                            Update
+                                        </button>
+                                        <button
+                                            className='manage-buttons'
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                openModal(
+                                                    <DeleteModal
+                                                        spotId={spot.id}
+                                                        handleDelete={() => handleDeleteButton(spot.id)}
+                                                    />
+                                                )
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
+                            </div>
+                        </>
+                    ))
+                ) : (
+                    <p className="no-listings">You don&apos;t have any spots listed yet.</p>
+                )}
+            </div>
+        </div>
 
 )
 }
