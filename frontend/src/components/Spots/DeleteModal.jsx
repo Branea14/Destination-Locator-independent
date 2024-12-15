e1d782836418cb03301750102458f48b4f770838
Erika <brandonerika96@gmail.com>
2024-12-15 00:06:31 -0500

    delete spot modal done

--- frontend/src/components/Spots/DeleteModal.jsx
+++ frontend/src/components/Spots/DeleteModal.jsx
@@ -1,9 +1,7 @@
-// import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-// import { removeSpot } from "../../store/spots";
+import './DeleteSpot.css';
 
 const DeleteModal = ({spotId, handleDelete}) => {
-    // const dispatch = useDispatch();
     const {closeModal} = useModal();
 
     const handleDeleteButton =  () => {
@@ -17,12 +15,18 @@ const DeleteModal = ({spotId, handleDelete}) => {
     }
 
     return (
-        <>
-            <h1>Confirm Delete</h1>
-            <p>Are you sure you want to remove this post from the listings?</p>
-            <button onClick={() => handleDeleteButton()}>Yes (Delete Spot)</button>
-            <button onClick={handleKeepButton}>No (Keep Spot)</button>
-        </>
+        <div className="modal-background">
+            <div className="delete-spot-modal">
+                <div className="delete-title">Confirm Delete</div>
+                <p>Are you sure you want to remove this post from the listings?</p>
+                <div className="delete-spot-buttons">
+
+                    <button className='delete-button' onClick={() => handleDeleteButton()}>Yes (Delete Spot)</button>
+                    <button className='keep-button' onClick={handleKeepButton}>No (Keep Spot)</button>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
